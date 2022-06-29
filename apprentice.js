c48637b6a0ee71b09288afb84f782df380614c5d
Skunk-Cabbage <93555636+Skunk-Cabbage@users.noreply.github.com>
2022-06-28 21:35:33 -0400

    Update apprentice.js

--- apprentice.js
+++ apprentice.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides',["#252525", "#ca6f6f", "#64986e", "#98966d", "#659abf", "#71739c", "#659999", "#7e7f7f", "#545555", "#ff9200", "#8dbc94", "#fefcb3", "#97bee1", "#979abe", "#51bdbd", "#fdffff"]);
 t.prefs_.set('foreground-color', "#c8c8c8");
 t.prefs_.set('background-color', "#313131");
-t.prefs_.set('cursor-color', "#839496");
+t.prefs_.set('cursor-color', "#c7c7c7");
